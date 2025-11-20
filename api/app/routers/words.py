c1a26447fbc90a5a9c1dab28229d5453c22394ce
philip-meie <mixexfp@gmail.com>
2025-11-20 22:25:01 +0700

    Update words.py

--- api/app/routers/words.py
+++ api/app/routers/words.py
@@ -1,25 +1,23 @@
 from fastapi import APIRouter, Depends, HTTPException
 import random
- 
-from app.database import get_db
+
 from app.models import Word
 from app.schemas import WordResponse
+
 from sqlalchemy.orm import Session
- 
+from app.database import get_db
+
 router = APIRouter()
- 
- 
-   
+
+
 @router.get("/word", response_model=WordResponse)
 def get_random_word(db: Session = Depends(get_db)):
     words = db.query(Word).all()
- 
-    if not words :
+    
+    if not words:
         raise HTTPException(
             status_code=404,
-            detail="No words available in database"
+            delattr= "No words available in database"
         )
-   
     random_word = random.choice(words)
- 
-    return random_word
+    return random_word 
